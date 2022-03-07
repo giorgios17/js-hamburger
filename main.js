144e3ce217d8503178f09551e55729d8f5af2756
giorgios17 <giosur@live.it>
2022-03-07 16:41:57 +0100

    done

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const hamburgerMenu = document.getElementByClassName('hamburger-menu');
+const hamburgerMenu = document.querySelector('.header-right > a');
+const closeHamburgerMenu = document.querySelector('.close')
+
 
 hamburgerMenu.addEventListener('click', function () {
-    console.log('click ok')
-    hamburgerMenu.classList.toggle('hamburger-menu.active');
+    console.log('click ok');
+    const hamburgerMenuDisplay = document.querySelector('.hamburger-menu').style.display = "block";
 
+})
+closeHamburgerMenu.addEventListener('click', function () {
+    console.log('click ok');
+    const hamburgerMenuDisplay = document.querySelector('.hamburger-menu').style.display = "none";
 
 })
