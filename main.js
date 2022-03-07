27424c3078d81fddbc8130c03fe2e52c136a07b8
giorgios17 <giosur@live.it>
2022-03-07 16:50:42 +0100

    aggiunta versione 2

--- main.js
+++ main.js
@@ -2,13 +2,36 @@ const hamburgerMenu = document.querySelector('.header-right > a');
 const closeHamburgerMenu = document.querySelector('.close')
 
 
+//VERSIONE 1
+
 hamburgerMenu.addEventListener('click', function () {
     console.log('click ok');
     const hamburgerMenuDisplay = document.querySelector('.hamburger-menu').style.display = "block";
 
 })
+
 closeHamburgerMenu.addEventListener('click', function () {
     console.log('click ok');
     const hamburgerMenuDisplay = document.querySelector('.hamburger-menu').style.display = "none";
 
 })
+
+
+
+//VERSIONE 2
+
+/*
+hamburgerMenu.addEventListener('click', function () {
+    console.log('click ok');
+    const hamburgerMenuDisplay = document.querySelector('.hamburger-menu');
+    hamburgerMenuDisplay.classList.add('active')
+
+})
+
+closeHamburgerMenu.addEventListener('click', function () {
+    console.log('click ok');
+    const hamburgerMenuDisplay = document.querySelector('.hamburger-menu');
+    hamburgerMenuDisplay.classList.remove('active')
+
+})
+*/
